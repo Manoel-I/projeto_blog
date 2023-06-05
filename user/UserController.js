e144f9fefc484b75225b7c6d3303d88489be5b77
Manoel-I <manoel.122112@outlook.com>
2023-06-05 14:01:17 -0300

    some changes

--- user/UserController.js
+++ user/UserController.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const User = require('./UserModel');
+const bcrypt = require('bcryptjs');
 
 
 router.get('/admin/users', (req , res) =>{
-    res.send("list of users");
+    User.findAll().then(users => {
+        res.render('admin/users/users.ejs', {users : users});
+    });
+    
 });
 
 
@@ -16,8 +20,30 @@ router.post('/users/create', (req, res)=>{
     var email = req.body.email;
     var password = req.body.password;
 
-    //User.create().then()
-    res.json({email, password});
+    User.findOne({
+        where : {
+            email : email
+        }
+    }).then(user =>{
+        if(user == undefined && email != ""){
+            var salt = bcrypt.genSaltSync(10); // a quantidade de "(rounds)" determina o número de iterações que serão realizadas durante o processo de hashing, aumentando assim a resistência a ataques de força bruta
+            var hash = bcrypt.hashSync(password, salt);
+            User.create({
+                    email : email,
+                password : hash
+            }).then(()=>{
+                res.json({email, hash});
+            }).catch((error)=>{
+                console.log(error);
+                res.redirect('/');
+            });
+        }else{
+            res.json({email: email, existe : true});
+        }
+    })
+
+    
+    
 });
 
 module.exports = router;
